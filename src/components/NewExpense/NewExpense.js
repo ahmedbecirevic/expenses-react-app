98e139eef787a8ccf11d50a0f6490410d3acb242
ahmedbecirevic <ahmed.becirevic@stu.ibu.edu.ba>
2021-08-10 20:44:40 +0200

    form  work

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,10 +1,17 @@
 import './NewExpense.css';
 import ExpenseForm from './ExpenseForm';
 
-const NewExpense = () => {
+const NewExpense = ({ onAddExpense }) => {
+  const saveExpsenseDataHandler = (enteredExepnseData) => {
+    const expenseData = {
+      ...enteredExepnseData,
+      id: Math.random().toString(),
+    };
+    onAddExpense(expenseData);
+  };
   return (
     <div className='new-expense'>
-      <ExpenseForm />
+      <ExpenseForm onSaveExpenseData={saveExpsenseDataHandler} />
     </div>
   );
 };
