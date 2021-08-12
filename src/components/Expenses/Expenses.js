378a095aa6dbc82a3d4d2f3f2f965bc8cc87b3b6
ahmedbecirevic <ahmed.becirevic@stu.ibu.edu.ba>
2021-08-12 17:31:22 +0200

    deal with empty lists

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -6,22 +6,28 @@ import Card from '../UI/Card';
 
 const Expenses = ({ items }) => {
   const [year, setYear] = useState(2021);
-
   const onYearFilterHandler = (selectedYear) => {
     setYear(Number(selectedYear));
   };
+
+  const expensesFiltered = items.filter(
+    (expense) => expense.date.getFullYear() === year
+  );
 
   return (
     <div>
       <Card className='expenses'>
         <ExpensesFilter defaultYear={year} onSelectYear={onYearFilterHandler} />
-        {items.map((expense) => (
-          <ExpenseItem
-            title={expense.title}
-            date={expense.date}
-            amount={expense.amount}
-          />
-        ))}
+        {expensesFiltered.length === 0 && <p>No expenses found</p>}
+        {expensesFiltered.length > 0 &&
+          expensesFiltered.map((expense) => (
+            <ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              date={expense.date}
+              amount={expense.amount}
+            />
+          ))}
       </Card>
     </div>
   );
